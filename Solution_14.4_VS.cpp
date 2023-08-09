b826b823928771797702fcbb56afe6c4b57069bf
Sergei Bess <sergeibesstuzhev1989@gmail.com>
2023-08-09 17:48:03 +0300

    Add length cout

--- Solution_14.4_VS.cpp
+++ Solution_14.4_VS.cpp
@@ -7,5 +7,6 @@ int main()
 {
     string s = "Sinhrophazotron";
     cout << s << "\n";
+    cout << "String length: " << s.length() << " letters\n";
     cout << "First letter: " << s[0] << " Last letter: " << s[s.length() - 1] << "\n";
 }
